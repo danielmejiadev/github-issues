8bb22df0805b6aa31d7f0b6ea66cfaca5ab48c3b
Daniel Mejia <luisdanielmejia@outlook.com>
2020-08-08 16:06:16 -0500

    AddStyling

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,17 @@ import AutocompleteProvider from './components/AutocompleteProvider';
 import SearchInput from './components/SearchInput';
 import Suggestions from './components/Suggestions';
 
+// Assets
+import logo from "./logo.svg";
+
 export function App() {
   return (
     <AutocompleteProvider>
       <div className="container">
+        <header className="App-header">
+          <img src={logo} alt="logo" />
+          <h1>React Github Issues</h1>
+        </header>
         <SearchInput />
         <Suggestions />
       </div>
