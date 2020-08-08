975b6c6a0c5794103e7f89c8ab0c1991b5f7a38c
Daniel Mejia <luisdanielmejia@outlook.com>
2020-08-08 14:03:52 -0500

    AddSearchInput

---
+++ src/components/SearchInput/index.spec.js
@@ -0,0 +1,33 @@
+// Dependencies
+import React from 'react';
+import { shallow } from 'enzyme';
+
+// Hooks
+import * as Hook from '../../hooks/useAutocomplete';
+
+// Under test
+import SearchInput from './index';
+
+describe('SearchInput', () => {
+  let component;
+  const state = {
+    search: 'search',
+    suggestions: [],
+    onSearch: jest.fn(),
+    handleKeyup: jest.fn(),
+  };
+
+  beforeEach(() => {
+    jest.spyOn(Hook, 'useTypeahead').mockReturnValue(state);
+    component = shallow(<SearchInput />)
+  });
+
+  it('should render correctly', () => {
+    const { value, className } = component
+      .find('input')
+      .props();
+
+    expect(value).toBe(state.search);
+    expect(className).toBe('search-input');
+  });
+});
